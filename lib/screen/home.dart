8ee47ffc02b396f34e26a6f81832a3c6cf311258
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-03-04 21:56:54 -0500

    second commit

---
+++ lib/screen/home.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rest Api Call'),
+      ),
+      body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final user = users[index];
+            final email = user['email'];
+            final name = user['name'];
+            return ListTile(
+              leading: CircleAvatar(child: Text('${index + 1}')),
+              title: Text(email),
+              subtitle: Text(name['first']),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchUsers,
+      ),
+    );
+  }
+
+  void fetchUsers() async {
+    print('fetch users found');
+    const url = 'https://randomuser.me/api/?results=100';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+  }
+}
